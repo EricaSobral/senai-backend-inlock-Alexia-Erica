84deb75766e535a54ff73cd9722beb2a6dc54d46
Erica Nascimento Sobral <44994599823@info.local>
2020-03-02 11:23:30 -0300

    Enviando banco de dados projeto InLock

---
+++ InLock_BD/InLock_BD_DQL_03.sql
@@ -0,0 +1,22 @@
+--DQL
+
+USE InLock_Games_Manha
+
+--Listar todos os usuários
+SELECT * FROM Usuario;
+
+-- Listar todos os estúdios
+SELECT * FROM Estudios;
+
+-- Listar todos os jogos
+SELECT * FROM Jogos;
+
+-- Listar todos os jogos e seus respectivos estúdios
+
+-- Buscar e trazer na lista todos os estúdios (mesmo que eles não contenham nenhum jogo de referência)
+
+-- Buscar um usuário por email e senha
+
+-- Buscar um jogo por IdJogo
+
+-- Buscar um estúdio por IdEstudio
